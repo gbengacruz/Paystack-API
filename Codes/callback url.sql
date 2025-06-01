e8742fa6b047c6cddb668213884b61051779d426
Gbenga Cruz <gbenga.cruz@gmail.com>
2025-06-01 12:45:37 +0100

    Update callback url.sql

--- Codes/callback url.sql
+++ Codes/callback url.sql
@@ -1,14 +1,16 @@
 DECLARE
-    l_url varchar2(2000);
-    l_app number := v('APP_ID');
-    l_session number := v('APP_SESSION');
+    l_url     VARCHAR2(2000);
+    l_app     NUMBER := v('APP_ID');
+    l_session NUMBER := v('APP_SESSION');
 BEGIN
-     :P1_REFNO  := DBMS_RANDOM.string('x', 4)|| S_TRAN_REFERENCE.nextval ; --(sys_guid()) ;
-        
-            l_url := APEX_UTIL.PREPARE_URL(
-        p_url => 'f?p=' || l_app || ':2:'||l_session||'::NO::',
-        p_url_charset => 'UTF-8',
-        p_checksum_type => 'SESSION');
-        
-        :P1_URL := 'https://r0mlg8oc0e1zzbk-dbdroplet01.adb.us-ashburn-1.oraclecloudapps.com'||l_url;
+    :p1_refno := dbms_random.string('x', 4)
+                 || s_tran_reference.nextval; --(sys_guid()) ;
+
+    l_url := apex_util.prepare_url(p_url => 'f?p='
+                                            || l_app
+                                            || ':2:'
+                                            || l_session
+                                            || '::NO::', p_url_charset => 'UTF-8', p_checksum_type => 'SESSION');
+
+    :p1_url := 'https://r0mlg8oc0e1zzbk-dbdroplet01.adb.us-ashburn-1.oraclecloudapps.com' || l_url; ---replace with your url base
 END;
