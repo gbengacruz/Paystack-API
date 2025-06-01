c13c4217ed5df2832532cca896808bb46870fca7
Gbenga Cruz <gbenga.cruz@gmail.com>
2025-06-01 12:48:42 +0100

    Update verify.sql

--- Codes/verify.sql
+++ Codes/verify.sql
@@ -1,47 +1,52 @@
-declare
-l_clob clob;
-p_body Clob;
-l_buffer varchar2(32767);
-l_amount number;
-l_offset number;
+DECLARE
+    l_clob   CLOB;
+    p_body   CLOB;
+    l_buffer VARCHAR2(32767);
+    l_amount NUMBER;
+    l_offset NUMBER;
 -- v_tran v_transactions%ROWTYPE;
 -- v_err v_99_ssu_log%ROWTYPE;
-begin
+BEGIN
+    apex_web_service.g_request_headers(1).name := 'Authorization';
+    apex_web_service.g_request_headers(1).value := 'Bearer ********************';
+    apex_web_service.g_request_headers(2).name := 'Content-Type';
+    apex_web_service.g_request_headers(2).value := 'application/json';
+    l_clob := apex_web_service.make_rest_request(p_url => 'https://api.paystack.co/transaction/verify/' || :p1_refno, p_http_method => 'GET'
+    , p_body => p_body);
 
-apex_web_service.g_request_headers(1).name := 'Authorization';
-apex_web_service.g_request_headers(1).value := 'Bearer ********************';
-apex_web_service.g_request_headers(2).name := 'Content-Type';
-apex_web_service.g_request_headers(2).value := 'application/json';
-l_clob := apex_web_service.make_rest_request(
-p_url => 'https://api.paystack.co/transaction/verify/'||:P1_REFNO,
-p_http_method => 'GET',
-p_body => p_body);
-l_amount := 32000;
-l_offset := 1;
-begin
-For i In (SELECT amount, status, reference
-FROM XMLTABLE(
-'/json/row'
-PASSING APEX_JSON.to_xmltype('['||l_clob||']')
-COLUMNS
-amount VARCHAR2(500) PATH '/row/data/amount',
-status VARCHAR2(500) PATH '/row/data/status',
-reference VARCHAR2(500) PATH '/row/data/reference')) loop
-IF i.status = 'success' THEN
-:P2_STA := i.status ;
-:P2_REFNO := i.reference;
-:P2_AMT := TO_CHAR(i.amount/100, '999G999G999G999G990D00');
+    l_amount := 32000;
+    l_offset := 1;
+    BEGIN
+        FOR i IN (
+            SELECT
+                amount,
+                status,
+                reference
+            FROM
+                XMLTABLE ( '/json/row'
+                        PASSING apex_json.to_xmltype('['
+                                                     || l_clob
+                                                     || ']')
+                    COLUMNS
+                        amount VARCHAR2(500) PATH '/row/data/amount',
+                        status VARCHAR2(500) PATH '/row/data/status',
+                        reference VARCHAR2(500) PATH '/row/data/reference'
+                )
+        ) LOOP
+            IF i.status = 'success' THEN
+                :p2_sta := i.status;
+                :p2_refno := i.reference;
+                :p2_amt := to_char(i.amount / 100, '999G999G999G999G990D00');
 ----- update transaction  status with a procedure
-ELSE
-:P2_STA_X := i.status ;
-:P2_REFNO_X := i.reference;
-:P2_AMT_X := TO_CHAR(i.amount/100, '999G999G999G999G990D00');
-END IF;
-end loop;
-exception
-when others then
-apex_error.add_error (
-p_message => 'Payment gateway not available',
-p_display_location => apex_error.c_on_error_page );
-end;
+            ELSE
+                :p2_sta_x := i.status;
+                :p2_refno_x := i.reference;
+                :p2_amt_x := to_char(i.amount / 100, '999G999G999G999G990D00');
+            END IF;
+        END LOOP;
+    EXCEPTION
+        WHEN OTHERS THEN
+            apex_error.add_error(p_message => 'Payment gateway not available', p_display_location => apex_error.c_on_error_page);
+    END;
+
 END;
